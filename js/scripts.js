176e3bcbcf7ba82f0ecfa91de9c15741cbe8908a
Yodel Guanzon <yodelguanzon@live.com>
2022-10-19 16:00:21 -0700

    Add form functionality
    Co-authored-by: Auston Moos <Austonhmoos2004@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
 window.onload = function() {
   const form = document.getElementById("quiz");
-  form.onsubmit = function(event) {
-    event.defaultPrevented();
-  }
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    document.getElementById("firstChar").setAttribute("class","hidden");
+    document.getElementById("secondChar").setAttribute("class","hidden");
+    document.getElementById("thirdChar").setAttribute("class","hidden");
+    document.getElementById("fourthChar").setAttribute("class","hidden");
+
+    let fn = document.getElementById("firstName").value.toLowerCase();
+    let age = parseInt(document.getElementById("age").value);
+    let color = document.querySelector("select").value.toLowerCase();
+    console.log(fn.charAt(0) + " test");
+    console.log(color + " test");
+    
+    if(fn.charAt(0) === "j" && age >= 59 && color === "blonde") {
+      document.getElementById("firstChar").removeAttribute("class","hidden");
+    }
+    else if(fn.charAt(0) != "j" && age > 15 && age < 59 && color === "red") {
+      document.getElementById("secondChar").removeAttribute("class","hidden");
+    }
+    else if(fn.charAt(0) === "a" && age > 18 && age <=36 && color === "black") {
+      document.getElementById("thirdChar").removeAttribute("class","hidden");
+    }
+    else {
+      document.getElementById("fourthChar").removeAttribute("class","hidden");
+    }
+   }
+  );
 };
